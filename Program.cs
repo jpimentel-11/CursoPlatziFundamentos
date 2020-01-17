2abf2c1b65504837d20109f8c435129b07881225
jazz pimentel <jazzpimentel09@gmail.com>
2020-01-16 18:45:32 -0700

    -Arrays y ciclos

--- Program.cs
+++ Program.cs
@@ -12,20 +12,60 @@ namespace Platzi2
             var escuela =new Escuela("Platzi Academy",1997,TiposEscuela.primaria,ciudad:"Culiacán",pais:"México");
             escuela.TipoEscuela= TiposEscuela.primaria;
             
-            
-            var curso1 =new Curso(){
+
+            var arregloCursos =new Curso[3];
+            arregloCursos[0]= new Curso(){
                 Nombre="101"
             };
 
-             var curso2 =new Curso(){
+            arregloCursos[1] =new Curso(){
                 Nombre="201"
             };
 
-             var curso3 =new Curso(){
+            arregloCursos[2]=new Curso(){
                 Nombre="301"
             };
 
             Console.WriteLine(escuela);
+            ImprimirCursos(arregloCursos);
+           
+        }
+
+        private static void ImprimirCursos(Curso[] arregloCursos)
+        {
+            //Recorriendo arreglo con for
+          /*  System.Console.WriteLine("=================FOR===================");
+             for(int i=0;i<arregloCursos.Length;i++)
+            {
+                Console.WriteLine("Nombre: "+arregloCursos[i].Nombre + ", Id: "+arregloCursos[0].UniqueId);
+            }*/
+
+
+            System.Console.WriteLine("=================FOR EACH===============");
+            foreach(var curso in arregloCursos)
+            {
+                Console.WriteLine("Nombre: "+curso.Nombre + ", Id: "+curso.UniqueId);
+
+            }
+            
+           /* var contador=0;
+            //Recorriendo arreglo con While
+            System.Console.WriteLine("=================WHILE===================");
+            while(contador<arregloCursos.Length)
+            {
+                Console.WriteLine("Nombre: "+arregloCursos[contador].Nombre + ", Id: "+arregloCursos[contador].UniqueId);
+                contador++;
+            }
+
+            //Recorriendo arreglo con dowhile
+            System.Console.WriteLine("================DO WHILE====================");
+            contador=0;
+            do
+            {
+                Console.WriteLine("Nombre: "+arregloCursos[contador].Nombre + ", Id: "+arregloCursos[contador].UniqueId);
+                contador++;
+            }while(contador<arregloCursos.Length);
+           */
         }
     }
 }
