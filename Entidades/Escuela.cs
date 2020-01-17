7a9e31f47194bd190caa3bbbeebf27331236402c
jazz pimentel <jazzpimentel09@gmail.com>
2020-01-16 17:38:14 -0700

    Agregue cursos a proyecto

--- Entidades/Escuela.cs
+++ Entidades/Escuela.cs
@@ -9,6 +9,7 @@ namespace coreEscuela.Entidades
       public string Pais {get; set;}
       public string Ciudad {get; set;}   
       public TiposEscuela TipoEscuela {get;set;}
+      
 
       public Escuela(string nombre, int año) => (Nombre,AñoDeCreacion) =(nombre,año);
 
--- Program.cs
+++ Program.cs
@@ -11,7 +11,20 @@ namespace Platzi2
            
             var escuela =new Escuela("Platzi Academy",1997,TiposEscuela.primaria,ciudad:"Culiacán",pais:"México");
             escuela.TipoEscuela= TiposEscuela.primaria;
-            Console.WriteLine(escuela);
+            
+            
+            var curso1 =new Curso(){
+                Nombre="101"
+            };
+
+             var curso2 =new Curso(){
+                Nombre="201"
+            };
+
+             var curso3 =new Curso(){
+                Nombre="301"
+            };
+
             Console.WriteLine(escuela);
         }
     }
